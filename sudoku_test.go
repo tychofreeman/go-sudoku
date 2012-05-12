f25223dfbda29911d313f974e66c9875209b39fd
Christopher Freeman <cwfreeman@gmail.com>
2012-05-12 01:10:49 -0500

    Changed to output [][]int due to test being written.

--- sudoku_test.go
+++ sudoku_test.go
@@ -4,13 +4,22 @@ import (
     "testing"
 )
 
-func Solution(board []int) []int {
+func Solution(board []int) [][]int {
     for i := range board {
         if board[i] == 0 {
             board[i] = 2
         }
     }
-    return board
+
+    result := make([][]int, len(board))
+    for i, v := range board {
+        result[i] = []int {v}
+    }
+    return result
+}
+
+func IsExactly(candidate []int, value int) bool {
+    return len(candidate) == 1 && candidate[0] == value
 }
 
 func TestZeroSizedBoard(t *testing.T) {
@@ -28,7 +37,7 @@ func TestOneSizedBoard(t *testing.T) {
         t.Errorf("Length should be 1, but was %v\n", result)
     }
 
-    if result[0] != 1 {
+    if !IsExactly(result[0], 1) {
         t.Errorf("Single value in result should be 1, but was %v\n", result[0])
     }
 }
@@ -37,11 +46,11 @@ func TestFindsMissingNumbersInList(t *testing.T) {
     input := []int {1, 0}
     result := Solution(input)
 
-    if result[0] != 1 {
+    if !IsExactly(result[0], 1) {
         t.Errorf("Known value should not be changed. Expected 1, but was %v\n", result[0])
     }
 
-    if result[1] != 2 {
+    if !IsExactly(result[1], 2) {
         t.Errorf("Unknown value was not found. Expected 2, but was %v\n", result[1])
     }
 }
@@ -50,11 +59,29 @@ func TestFindsMissingNumbersInList2(t *testing.T) {
     input := []int {0, 1}
     result := Solution(input)
 
-    if result[1] != 1 {
+    if !IsExactly(result[1], 1) {
         t.Errorf("Known value should not be changed. Expected 1, but was %v\n", result[1])
     }
 
-    if result[0] != 2 {
+    if !IsExactly(result[0], 2) {
         t.Errorf("Unkonwn value was not found. Expected 2, but was %v\n", result[1])
     }
 }
+
+/*
+func TestFindsMultipleMissingNumbersInLongerList(t *testing.T) {
+    input := []int {1, 0, 0}
+    result := Solution(input)
+
+    if !IsExactly(result[0], 1) {
+        t.Errorf("Known value should not be changed. Expected 1, but was %v\n", result[0])
+    }
+
+    if !HasAllOf(result[1], []int {2,3}) {
+        t.Errorf("Undecidable value should have all possible values. Expected {2,3}, but was %v\n", result[1])
+    }
+    if !HasAllOf(result[1], []int {2,3}) {
+        t.Errorf("Undecidable value should have all possible values. Expected {2,3}, but was %v\n", result[2])
+    }
+}
+*/
