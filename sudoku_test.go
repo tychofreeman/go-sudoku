3bca491b9b83d03c5209cfdf2543a5fb86cb3943
Christopher Freeman <cwfreeman@gmail.com>
2012-05-12 00:08:05 -0500

    Second, another degenerate test.

--- sudoku_test.go
+++ sudoku_test.go
@@ -5,12 +5,25 @@ import (
 )
 
 func Solution(board []int) []int {
-    return nil
+    return board
 }
 
 func TestZeroSizedBoard(t *testing.T) {
     emptyBoard := []int {}
     if len(Solution(emptyBoard)) != 0 {
         t.Fail()
+    }
+}
+
+func TestOneSizedBoard(t *testing.T) {
+    oneSized := []int {1}
+    result := Solution(oneSized)
+
+    if len(result) != 1 {
+        t.Errorf("Length should be 1, but was %v\n", result)
+    }
+
+    if result[0] != 1 {
+        t.Errorf("Single value in result should be 1, but was %v\n", result[0])
     }
 }
