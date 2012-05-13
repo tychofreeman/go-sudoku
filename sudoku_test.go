7735662a1e47ac9e0ff61c60d1cb31fff63e6de6
Christopher Freeman <cwfreeman@gmail.com>
2012-05-13 00:55:56 -0500

    Finished ugly refactor. Should probably redo it.

--- sudoku_test.go
+++ sudoku_test.go
@@ -4,31 +4,40 @@ import (
     "testing"
 )
 
-func GetMissingValue(board []int) []int {
-    missing := make([]int, len(board))
-    missingValue := make([]int, 0)
+func MapMissingValues(board [][]int) [][]int {
+    found := make([]int, len(board))
     for _, v := range board {
-        if v > 0 {
-            missing[v-1] = v
+        if len(v) >= 1 {
+            found[v[0]-1] = v[0]
         }
     }
+
+    notFound := make([]int, 0)
+    for i, v := range found {
+        if v == 0 {
+            notFound = append(notFound, i + 1)
+        }
+    }
+
+    missingValue := make([][]int, len(board))
     for i := range board {
-        if missing[i] == 0 {
-            missingValue = append(missingValue, i+1)
+        missingValue[i] = make([]int, 0)
+        if len(board[i]) == 0 {
+            missingValue[i] = notFound
         }
     }
     return missingValue
 }
 
-func Solution(board []int) [][]int {
-    missingValue := GetMissingValue(board)
+func Solution(board [][]int) [][]int {
+    missingValue := MapMissingValues(board)
 
     result := make([][]int, len(board))
     for i, v := range board {
-        if v == 0 {
-            result[i] = missingValue
+        if len(v) == 0 {
+            result[i] = missingValue[i]
         } else {
-            result[i] = []int {v}
+            result[i] = v
         }
     }
     return result
@@ -54,14 +63,14 @@ func HasAllOf(candidate []int, values []int) bool {
 }
 
 func TestZeroSizedBoard(t *testing.T) {
-    emptyBoard := []int {}
+    emptyBoard := [][]int {}
     if len(Solution(emptyBoard)) != 0 {
         t.Fail()
     }
 }
 
 func TestOneSizedBoard(t *testing.T) {
-    oneSized := []int {1}
+    oneSized := [][]int {[]int{1}}
     result := Solution(oneSized)
 
     if len(result) != 1 {
@@ -74,7 +83,7 @@ func TestOneSizedBoard(t *testing.T) {
 }
 
 func TestFindsMissingNumbersInList(t *testing.T) {
-    input := []int {1, 0}
+    input := [][]int {[]int{1}, []int{}}
     result := Solution(input)
 
     if !IsExactly(result[0], 1) {
@@ -87,7 +96,7 @@ func TestFindsMissingNumbersInList(t *testing.T) {
 }
 
 func TestFindsMissingNumbersInList2(t *testing.T) {
-    input := []int {0, 1}
+    input := [][]int {[]int{}, []int{1}}
     result := Solution(input)
 
     if !IsExactly(result[1], 1) {
@@ -95,12 +104,12 @@ func TestFindsMissingNumbersInList2(t *testing.T) {
     }
 
     if !IsExactly(result[0], 2) {
-        t.Errorf("Unkonwn value was not found. Expected 2, but was %v\n", result[1])
+        t.Errorf("Unknown value was not found. Expected 2, but was %v\n", result[0])
     }
 }
 
 func TestFindsMissingNumberInSize3List(t *testing.T) {
-    input := []int {1, 2, 0}
+    input := [][]int {[]int{1}, []int{2}, []int{}}
     result := Solution(input)
     if !IsExactly(result[0], 1) || !IsExactly(result[1], 2) {
         t.Errorf("Known values should not be changed.\n")
@@ -113,7 +122,23 @@ func TestFindsMissingNumberInSize3List(t *testing.T) {
 }
 
 func TestFindsMultipleMissingNumbersInLongerList(t *testing.T) {
-    input := []int {1, 0, 0}
+    input := [][]int {[]int{1}, []int{}, []int{}}
+    result := Solution(input)
+
+    if !IsExactly(result[0], 1) {
+        t.Errorf("Known value should not be changed. Expected 1, but was %v\n", result[0])
+    }
+
+    if !HasAllOf(result[1], []int {2,3}) {
+        t.Errorf("Undecidable value should have all possible values. Expected {2,3}, but was %v\n", result[1])
+    }
+    if !HasAllOf(result[1], []int {2,3}) {
+        t.Errorf("Undecidable value should have all possible values. Expected {2,3}, but was %v\n", result[2])
+    }
+}
+
+func TestReducesMissingNumbersIfNumIsPresent(t *testing.T) {
+    input := [][]int {[]int{1}, []int{2,1,3}, []int{2,1,3}}
     result := Solution(input)
 
     if !IsExactly(result[0], 1) {
